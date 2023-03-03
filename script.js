cd839673caefc76613298b2a3cbacfa20508cbda
ashley speight <ashleyspeight86@gmail.com>
2023-03-03 23:51:11 +0100

    basic functions working

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const result = document.getElementById("results");
 const digits = document.getElementsByClassName("digit");
 const operators = document.getElementsByClassName("op");
 const calfunc = document.getElementsByClassName("calfunction");
+const eq = document.getElementById("=");
 
 let displayValue = 0;
 let value1 = 0;
 let value2 = 0;
 let finalResult = 0;
 let lastOp = "";
+let pressedEquals = false;
 
 result.innerText = displayValue;
 
@@ -27,28 +29,29 @@ for (const digit of digits) {
 // operator actions
 for (const op of operators) {
   op.addEventListener("click", (e) => {
-    result.innerText = e.target.id;
-    if (e.target.id != "=") {
-      lastOp = e.target.id;
-      result.innerText = e.target.id;
-    }
-
     if (value1 == 0) {
       value1 = displayValue;
       displayValue = 0;
       result.innerText = e.target.id;
-    } else if (value2 == 0) {
-      value2 = displayValue;
+      lastOp = e.target.id;
+    } else {
+      lastOp = e.target.id;
+      result.innerText = lastOp;
       displayValue = 0;
-      result.innerText = e.target.id;
-      finalResult = getResult(value1, lastOp, value2);
-      result.innerText = finalResult;
-      value1 = finalResult;
-      value2 = 0;
-      finalResult = 0;
     }
   });
 }
+
+eq.addEventListener("click", (e) => {
+  if (value2 == 0) {
+    value2 = displayValue;
+    displayValue = 0;
+    finalResult = getResult(value1, lastOp, value2);
+    value1 = finalResult;
+    value2 = 0;
+    result.innerText = finalResult;
+  }
+});
 
 // Get results when we have two numbers and an operation
 function getResult(value1, operator, value2) {

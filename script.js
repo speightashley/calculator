d376246cb4aba254e1b3da0749a1ee71b430e6ee
ashley speight <ashleyspeight86@gmail.com>
2023-03-05 19:09:13 +0100

    Finish working version

--- script.js
+++ script.js
@@ -9,9 +9,23 @@ let value1 = 0;
 let value2 = 0;
 let finalResult = 0;
 let lastOp = "";
-let pressedEquals = false;
+let equalsPressed = false;
 
 result.innerText = displayValue;
+// TODO : Handle chaining of values
+
+/*
+
+Without pressing =
+1. enter a number - Store as displayValue, show on results inner text
+2. push an operator - Move display value into value1 -> store operator in lastOp, Change display value to lastOp, result inner text becomes operator
+3. reset display value to 0
+4. enter another number - store in display value
+4. push another operator - store display value in value2, reset display value to 0, calculate the result
+    
+
+
+*/
 
 // Get digits
 for (const digit of digits) {
@@ -34,6 +48,7 @@ for (const fun of calfunc) {
       lastOp = "";
       value1 = 0;
       value2 = 0;
+      finalResult = 0;
       result.innerText = displayValue;
     }
   });
@@ -42,15 +57,25 @@ for (const fun of calfunc) {
 // operator actions
 for (const op of operators) {
   op.addEventListener("click", (e) => {
-    if (value1 == 0) {
-      value1 = displayValue;
-      displayValue = 0;
-      result.innerText = e.target.id;
-      lastOp = e.target.id;
-    } else {
-      lastOp = e.target.id;
-      result.innerText = lastOp;
-      displayValue = 0;
+    if (equalsPressed == false) {
+      if (value1 == 0) {
+        value1 = displayValue;
+        displayValue = e.target.id;
+        lastOp = displayValue;
+        result.innerText = displayValue;
+        displayValue = 0;
+      } else if (value1 != 0) {
+        value2 = displayValue;
+        displayValue = 0;
+        result.innerText = value2;
+        finalResult = getResult(value1, lastOp, value2);
+        lastOp = e.target.id;
+        displayValue = finalResult;
+        result.innerText = displayValue;
+        value2 = 0;
+        value1 = displayValue;
+        displayValue = 0;
+      }
     }
   });
 }
@@ -60,9 +85,19 @@ eq.addEventListener("click", (e) => {
     value2 = displayValue;
     displayValue = 0;
     finalResult = getResult(value1, lastOp, value2);
-    value1 = finalResult;
+    displayValue = finalResult;
+    value1 = 0;
     value2 = 0;
+    finalResult = 0;
+
+    result.innerText = displayValue;
+  } else {
+    finalResult = getResult(value1, lastOp, value2);
     result.innerText = finalResult;
+    value2 = 0;
+    value1 = finalResult;
+    displayValue = 0;
+    finalResult = 0;
   }
 });
 
